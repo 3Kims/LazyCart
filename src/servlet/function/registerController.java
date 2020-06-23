d1ff958c7c85f867191a05e4f3e04a8d2230dd62
windect <rbghksdl14@gmail.com>
2020-06-23 14:27:18 +0900

    ADD_vo_DAO_controller
    ADD VO,DAO,CONTROLLER

---
+++ src/servlet/function/registerController.java
@@ -0,0 +1,20 @@
+package servlet.function;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import servlet.controller.Controller;
+import servlet.controller.ModelAndView;
+
+public class registerController implements Controller {
+
+	@Override
+	public ModelAndView handle(HttpServletRequest request, HttpServletResponse response) {
+		// TODO Auto-generated method stub
+		String path = "registerSuccess.jsp";
+		
+		
+		return new ModelAndView(path);
+	}
+
+}
