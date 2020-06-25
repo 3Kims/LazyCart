048705ea75d212a8857658bafe3b2ef27c69430e
baleineindigo <39041622+baleineindigo@users.noreply.github.com>
2020-06-25 15:13:40 +0900

    upload
    AllProductController
    >> 당장 쓰지는 않지만, 혹시 쓸 일 있을까봐 만들어두었습니다.
    loginController
    >> 상품정보 세션에 담아서 뿌리는 작업만 남았습니다.
    DAOImpl
    >> 치종님이 공유해주신 메소드에 추가.
    화면단 jsp 파일이 좀 많아여
    registerController
    >> 파일명 컨벤션때무에 RegisterController로 고쳤습니다. 둘 중 하나는 소문자로 시작하는 파일은 삭제해도 무방합니다.

--- src/servlet/function/LoginController.java
+++ src/servlet/function/LoginController.java
@@ -1,20 +1,31 @@
 package servlet.function;
 
+import java.sql.SQLException;
+
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-
+import javax.servlet.http.HttpSession;
+import model.customerVO;
+import model.ezbasketDAOImpl;
 import servlet.controller.ModelAndView;
 
 public class LoginController implements Controller {
 
 	@Override
-	public ModelAndView handle(HttpServletRequest request, HttpServletResponse response) {
-		// TODO Auto-generated method stub
+	public ModelAndView handle(HttpServletRequest request, HttpServletResponse response) throws SQLException {
 		String id = request.getParameter("id");
 		String password = request.getParameter("password");
-		String path = "home.html";
-		
+		String path="main.jsp";
+		// 2. dao 리턴 받고 biz로직 호출
+		customerVO customer=ezbasketDAOImpl.getInstance().login(id, password);
+		System.out.println("로그인 서블릿 vo:"+customer);
+		if(customer == null) path="LoginError.jsp";
+		else {
+			HttpSession session=request.getSession();
+			session.setAttribute("customer", customer);
+			System.out.println("세션에 저장");
+		}
+		//3. 값 바인딩	
 		return new ModelAndView(path);
 	}
-
 }
