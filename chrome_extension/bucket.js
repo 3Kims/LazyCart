11d4cb94e84b0765bc7ed04c599c2e17bdb77d65
ehehehe95 <53392103+ehehehe95@users.noreply.github.com>
2020-06-25 16:54:01 +0900

    chrome_extension/version1.02
    전체 iframe 사용
    현재 페이지 url 자동 추가 기능 추가

---
+++ chrome_extension/bucket.js
@@ -0,0 +1,7 @@
+document.getElementById("addCart").addEventListener("click", addCart);
+
+
+function addCart() {
+	var iframe = document.getElementById("iframe_cart");
+	iframe.src=("test.html");
+}
